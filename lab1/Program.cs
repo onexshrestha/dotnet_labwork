3b06a763eb9d3f4f8a00d4319efa078a22628ae1
test <test@gmail.com>
2026-08-09 10:57:37 +0545

    Add lab 1

---
+++ lab1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using lab1;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IO.first();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab1/student.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace lab1
+{
+     public class student
+    {
+        public string name;
+        public int roll;
+    }
+}
